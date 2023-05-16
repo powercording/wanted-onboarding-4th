0944dba3b1434e9e057f309e7c5f0e663c876abf
powercording <adoim@naver.com>
2023-05-16 19:04:39 +0900

    Refactor: 옵저버가 인덱스를 증가시지키 않게 인자 전달

--- src/hooks/useObserver.tsx
+++ src/hooks/useObserver.tsx
@@ -1,17 +1,18 @@
 import React, { useRef, useCallback } from 'react';
 
-const INITIALINDEX = 0;
+export const INITIALINDEX = 1;
 
-export default function useObserver() {
+export default function useObserver(shouldStop: boolean) {
   const [isIntersecting, setIsIntersecting] = React.useState(false);
   const [index, setIndex] = React.useState(INITIALINDEX);
+
   const intersection = useRef<IntersectionObserver | null>(null);
 
   const observer = useCallback((node: any) => {
     if (intersection.current) intersection.current.disconnect();
 
     intersection.current = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
-      if (entries[0].isIntersecting) {
+      if (entries[0].isIntersecting && !shouldStop) {
         setIsIntersecting(true);
         setIndex((prev: number) => prev + 1);
       }
@@ -23,5 +24,10 @@ export default function useObserver() {
 
     if (node) intersection.current.observe(node as unknown as Element);
   }, []);
-  return { index, observer, isIntersecting };
+  return {
+    index,
+    setIndex,
+    observer,
+    isIntersecting,
+  };
 }
